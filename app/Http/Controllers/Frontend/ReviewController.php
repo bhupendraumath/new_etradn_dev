4bb035b9883280642f92280149dfde377b79759c
bhupendraumath <bhupendraumath999@gmail.com>
2022-04-05 01:14:10 +0530

    work in resolve rating issue in server

--- app/Http/Controllers/Frontend/ReviewController.php
+++ app/Http/Controllers/Frontend/ReviewController.php
@@ -4,15 +4,10 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Product;
-use App\Models\Category;
-use App\Models\Brand;
 use App\Models\ProductReview;
 use App\Models\OrderItem;
-
-use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
-use DB;
+
 
 class ReviewController extends Controller
 {
@@ -22,77 +17,76 @@ class ReviewController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function checking_order_existing(Request $request){
-        
-        $product_review=ProductReview::where(['buyerId'=>$request->buyerId,
-                    'sellerId'=>$request->seller_id,
-                    'productId'=>$request->productId
-                    ])
-                    ->get();
+    public function checking_order_existing(Request $request)
+    {
 
-                    
-        if(count($product_review)==0){
-           
+        $product_review = ProductReview::where([
+            'buyerId' => $request->buyerId,
+            'sellerId' => $request->seller_id,
+            'productId' => $request->productId
+        ])->get();
 
-            $buyerId=$request->buyerId;
-            $order_item=OrderItem::with(['getOrder'=>function($q) use($buyerId){
-                $q->where("buyer_id",$buyerId);
+
+        if (count($product_review) == 0) {
+
+
+            $buyerId = $request->buyerId;
+            $order_item = OrderItem::with(['getOrder' => function ($q) use ($buyerId) {
+                $q->where("buyer_id", $buyerId);
             }])
-            ->where('seller_id',$request->seller_id)
-            ->get();
+                ->where('seller_id', $request->seller_id)
+                ->get();
 
-            $order_id='';
+            $order_id = '';
 
 
 
-            if(!empty($order_item)){
+            if (!empty($order_item)) {
 
-                for($i=0;$i<count($order_item);$i++){
-                   
-                    if(!empty($order_item[$i]['product_detail_1'])){
+                for ($i = 0; $i < count($order_item); $i++) {
 
-                    $encode=json_encode($order_item[$i]['product_detail_1']);
-                    $decode=json_decode($encode,true);
+                    if (!empty($order_item[$i]['product_detail_1'])) {
 
-                    $my_str_arr = preg_split ("/,/", $decode);
-                    $id_convert=preg_split ("/{/", $my_str_arr[0]);
-                    $id_remove=  preg_split ('/"id":/', $id_convert[1]);
-                    $id_remove2=  preg_split ('/"/', $id_remove[1]);
-                    // $product_id=$id_remove2[1];
-                    if(!empty($id_remove2[0])){
-                        $product_id=$id_remove2[0];
+                        $encode = json_encode($order_item[$i]['product_detail_1']);
+                        $decode = json_decode($encode, true);
+
+                        $my_str_arr = preg_split("/,/", $decode);
+                        $id_convert = preg_split("/{/", $my_str_arr[0]);
+                        $id_remove =  preg_split('/"id":/', $id_convert[1]);
+                        $id_remove2 =  preg_split('/"/', $id_remove[1]);
+                        // $product_id=$id_remove2[1];
+                        if (!empty($id_remove2[0])) {
+                            $product_id = $id_remove2[0];
+                        } else {
+                            $product_id = $id_remove2[1];
                         }
-                        else{                            
-                        $product_id=$id_remove2[1];
+
+                        if ($product_id == $request->productId) {
+                            $order_id = $order_item[$i]['id'];
+                            // return $order_id;
+                            return response()->json(
+                                [
+                                    'success' => true,
+                                    'message' => 'Order id exist',
+                                    'data' =>
+                                    ['orderId' => $order_id]
+
+                                ]
+                            );
+                        } else {
+                            if ($i == count($order_item) - 1)
+                                return response()->json(
+                                    [
+                                        'success' => false,
+                                        'message' => 'Order id not exist',
+                                        'data' =>
+                                        ['orderId' => $order_id]
+
+                                    ]
+                                );
                         }
 
-                    if($product_id==$request->productId){
-                        $order_id=$order_item[$i]['id'];
-                        // return $order_id;
-                        return response()->json(
-                            [
-                                'success' => true,
-                                'message'=>'Order id exist',
-                                'data' =>
-                                ['orderId'=>$order_id]
-                                
-                            ]
-                        );
-                    }
-                    else{
-                        if($i==count($order_item)-1)
-                        return response()->json(
-                            [
-                                'success' => false,
-                                'message'=>'Order id not exist',
-                                'data' =>
-                                ['orderId'=>$order_id]
-                                
-                            ]
-                        );
-                    }
-                    
-                   /* if($product_id==$request->productId){
+                        /* if($product_id==$request->productId){
 
                         $order_id=$order_item[$i]['id'];
                         // return $order_id;
@@ -109,65 +103,63 @@ class ReviewController extends Controller
                     }
                    */
                     }
-                }               
-
-            }
-            else{
+                }
+            } else {
                 // return $order_id;
                 return response()->json(
                     [
                         'success' => false,
-                        'message'=>'Order id not exist',
+                        'message' => 'Order id not exist',
                         'data' =>
-                        ['orderId'=>$order_id]
-                        
+                        ['orderId' => $order_id]
+
                     ]
                 );
             }
-        }
-        else{
+        } else {
             return response()->json(
                 [
                     'success' => false,
-                    'message'=>'You are already submitted Review & Rating.',
-                    'data' =>$product_review
-                    
+                    'message' => 'You are already submitted Review & Rating.',
+                    'data' => $product_review
+
                 ]
             );
         }
     }
 
-    public function submit_rating(Request $request ){
+    public function submit_rating(Request $request)
+    {
 
-        $product=ProductReview::create($request->all());
+        $product = ProductReview::create($request->all());
 
-        if(!empty($product)){
+        if (!empty($product)) {
             return response()->json(
                 [
                     'success' => true,
-                    'message'=>'Your Review & Rating Successfully Submitted',
+                    'message' => 'Your Review & Rating Successfully Submitted',
                     'data' =>
                     [
                         'data' => $product
                     ]
                 ]
             );
-        }
-        else{
+        } else {
 
             return response()->json(
                 [
                     'success' => false,
-                    'message'=>'Failed try again leter',
-                    
-                     'data' => $product
-                    
+                    'message' => 'Failed try again leter',
+
+                    'data' => $product
+
                 ]
             );
         }
     }
 
-    public function submit_rating_load(Request $request){
+    public function submit_rating_load(Request $request)
+    {
 
         $average_rating = 0;
         $total_review = 0;
@@ -179,85 +171,79 @@ class ReviewController extends Controller
         $total_user_rating = 0;
         $review_content = array();
 
-        $product=ProductReview::with("user")->where("productId",$request->id)
-        ->orderBy("id",'desc')
-        ->get();
+        $product = ProductReview::with("user")->where("productId", $request->id)
+            ->orderBy("id", 'desc')
+            ->get();
 
 
         // print_r($product);die;
-        foreach($product as $row)
-        {
+        foreach ($product as $row) {
             $review_content[] = array(
-                'user_name'		=>	$row->user->firstName,
-                'user_review'	=>	$row["description"],
-                'rating'		=>	$row["rating"],
+                'user_name'        =>    $row->user->firstName,
+                'user_review'    =>    $row["description"],
+                'rating'        =>    $row["rating"],
                 // 'datetime'		=>	date('l jS, F Y h:i:s A', $row["createdDate"])
-                'datetime'		=>	$row["createdDate"]
-                
+                'datetime'        =>    $row["createdDate"]
+
             );
 
-            if($row["rating"] == '5')
-            {
+            if ($row["rating"] == '5') {
                 $five_star_review++;
             }
 
-            if($row["rating"] == '4')
-            {
+            if ($row["rating"] == '4') {
                 $four_star_review++;
             }
 
-            if($row["rating"] == '3')
-            {
+            if ($row["rating"] == '3') {
                 $three_star_review++;
             }
 
-            if($row["rating"] == '2')
-            {
+            if ($row["rating"] == '2') {
                 $two_star_review++;
             }
 
-            if($row["rating"] == '1')
-            {
+            if ($row["rating"] == '1') {
                 $one_star_review++;
             }
 
             $total_review++;
 
             $total_user_rating = $total_user_rating + $row["rating"];
-
+        }
+        $average_rating = 0;
+        if (!empty($total_review)) {
+            $average_rating = $total_user_rating / $total_review;
         }
 
-        $average_rating = $total_user_rating / $total_review;
-
         $output = array(
-            'average_rating'	=>	number_format($average_rating, 1),
-            'total_review'		=>	$total_review,
-            'five_star_review'	=>	$five_star_review,
-            'four_star_review'	=>	$four_star_review,
-            'three_star_review'	=>	$three_star_review,
-            'two_star_review'	=>	$two_star_review,
-            'one_star_review'	=>	$one_star_review,
-            'review_data'		=>	$review_content
+            'average_rating'    =>    number_format($average_rating, 1),
+            'total_review'        =>    $total_review,
+            'five_star_review'    =>    $five_star_review,
+            'four_star_review'    =>    $four_star_review,
+            'three_star_review'    =>    $three_star_review,
+            'two_star_review'    =>    $two_star_review,
+            'one_star_review'    =>    $one_star_review,
+            'review_data'        =>    $review_content
         );
 
         // $product=ProductReview::create($request->all());
 
-        if(!empty($product)){
+        if (!empty($product)) {
             return response()->json(
                 [
                     'success' => true,
-                    'message'=>'Success',
-                    'data' =>$output
-                  
+                    'message' => 'Success',
+                    'data' => $output
+
                 ]
             );
-        }
-        else{
+        } else {
 
             return response()->json(
                 [
                     'success' => false,
-                    'message'=>'Failed try again leter',
+                    'message' => 'Failed try again leter',
                     'data' =>
                     [
                         'data' => $output
@@ -268,21 +254,17 @@ class ReviewController extends Controller
     }
     public function review_rating()
     {
-    
-            $userid=Auth::user()->id;
-            $productlist = ProductReview::where('sellerid',$userid)
+
+        $userid = Auth::user()->id;
+        $productlist = ProductReview::where('sellerid', $userid)
             ->with('product')
             ->paginate(4);
 
-            return view('frontend/seller/review-rating',
+        return view(
+            'frontend/seller/review-rating',
             compact(
                 'productlist',
-            ));
-
+            )
+        );
     }
-
-
-
-
-
 }
